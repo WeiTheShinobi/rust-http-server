26fd373c22201203e215419e98ddc836cff30dcd
WeiTheShinobi <weitheshinobi@gmail.com>
2023-03-30 19:59:36 +0800

    Build a simple handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{BufRead, BufReader};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_read = BufReader::new(stream);
+    let http_request : Vec<_> = buf_read
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("請求：{:#?}", http_request);
 }
