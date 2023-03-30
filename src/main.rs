3ef3e11dc70badabaee359797d9d2859615021f1
WeiTheShinobi <weitheshinobi@gmail.com>
2023-03-30 21:18:50 +0800

    Add response and html file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{BufRead, BufReader};
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -12,12 +13,18 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_read = BufReader::new(stream);
-    let http_request : Vec<_> = buf_read
+    let buf_read = BufReader::new(&mut stream);
+    let _http_request : Vec<_> = buf_read
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("請求：{:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    println!("{}", response);
+    stream.write_all(response.as_bytes()).unwrap();
 }
